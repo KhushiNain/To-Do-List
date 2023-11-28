3b3d895f8c511dedb6dcf40342af251d26bdfe38
unknown <khushi.nain@kalvium.community>
2023-11-28 16:07:31 +0530

    Functional to do list

--- script.js
+++ script.js
@@ -1,72 +1,67 @@
 let inputTag=document.getElementById("input");
-let $toDoList=document.getElementById("todolist");
-let todoListArr=[];
+let $todoList=document.getElementById("todolist");
+let todoListArr=[]
 
 
-function addToList(){
-
+function AddToList(){
     let task=inputTag.value;
     todoListArr.push(task);
-
-    showList();
+    console.log(todoListArr);
+    
+    showlist();
 
     inputTag.value="";
+
 }
 
-function showList(){
+function showlist(){
 
-    $toDoList.innerHTML=null;
+    $todoList.innerHTML=null;
 
     todoListArr.forEach((ele,idx)=>{
 
-    let li=document.createElement("li")
-    li.style.position="relative";
-    let text=document.createElement("span");
-    let box=document.createElement("span");
-    box.style.marginLeft="150px";
-    box.style.position="absolute";
-    box.style.right="15px";
-    let span1=document.createElement("span");
+        let li=document.createElement("li");
+        li.style.position="relative";
+        let text=document.createElement("span");
+        let box=document.createElement("span");
+        box.style.scrollMarginLeft="150px";
+        box.style.position="absolute";
+        box.style.right="15px";
+        let span1=document.createElement("span");
+        span1.addEventListener("click", function(){
+            deleteTask(idx)
+        })
+        let span2=document.createElement("span");
+        let span3=document.createElement("span");
+        span3.addEventListener("click",function(){
+            editTask(idx);
+        })
 
-    // making delete option work
-    span1.addEventListener("click",function(){
-        deleteTask(idx);
-    })
+        text.innerText= ele;
 
-    let span2=document.createElement("span");
-    let span3=document.createElement("span");  
-
-    // making edit option work
-    span2.addEventListener("click",function(){
-        editTask(idx);
-    })
-
-
-
-    text.innerText=task;
-    span1.innerHTML="<span>&times<span/>";
-    span2.innerText="|";
-    span3.innerText="Edit";
-    box.append(span1,span2,span3);
-    li.append(text,box)
-    $toDoList.append(li);
-
-    })
+        
+        span1.innerHTML="<span>&times<span/>"
+        span2.innerText="|";
+        span3.innerText="Edit";
+        box.append(span1,span2,span3);
+        li.append(text,box);
+        $todoList.append(li);
+    
+    });
 }
-
-
-// fuction to delete a list item
-function deleteTask(index){
-    todoListArr.splice(index,1);
-    showList();
-
-}
-// function to edit a list item
-function editTask(index){
-    let newTask=prompt("Edit the to do Iteam: ");
-    if(newTask==""){
-        alert("Please update the task")
+    function deleteTask(index){
+        todoListArr.splice(index,1,);
+        console.log(todoListArr);
+        showlist()
     }
-    todoListArr.splice(index,1,newTask);
-    showList();
-}
+
+
+    function editTask(index){
+        let newTask=prompt("Edit the to-do List");
+        if(newTask==""){
+            alert("please update the task");
+        }
+        todoListArr.splice(index,1,newTask);
+        // console.log(todoListArr);
+        showlist();
+    }
